68af938e6a2dbf4ab09903c2f3bc8905ebbd3301
ANUJDEO <68499377+Anuj1235@users.noreply.github.com>
2023-06-22 16:35:06 +0530

    Update LoginPageTest.java
    Changes the title

--- src/test/java/Testcases/LoginPageTest.java
+++ src/test/java/Testcases/LoginPageTest.java
@@ -28,7 +28,7 @@ public class LoginPageTest extends TestBase{
 	@Test(priority=1)
 	public void loginPageTitleTest(){
 		String title = loginPage.validateLoginPageTitle();
-		Assert.assertEquals(title, "#1 Free CRM for Any Business: Online Customer Relationship Software");
+		Assert.assertEquals(title, "Free CRM - CRM software for customer relationship management, sales, and support.");
 	}
 	
 	@Test(priority=2)
